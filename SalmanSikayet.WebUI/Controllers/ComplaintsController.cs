62403061522e8368346e0c9a52306d4139c031bd
HakanŞahin <abc@a.a>
2022-10-03 11:08:13 +0300

    Swal eklendi. Mesaj düzenlendi

--- SalmanSikayet.WebUI/Controllers/ComplaintsController.cs
+++ SalmanSikayet.WebUI/Controllers/ComplaintsController.cs
@@ -21,13 +21,16 @@ namespace SalmanSikayet.WebUI.Controllers
         [HttpPost]
         public IActionResult Index(ComplaintModel complaint)
         {
+            
             if (ModelState.IsValid)
             {
                 StringContent content = new StringContent(JsonConvert.SerializeObject(complaint), Encoding.UTF8, "application/json");
                 client.BaseAddress = new Uri("http://localhost:25011");
                 var re = client.PostAsync($"{"/api/Complaints"}", content);
-                return RedirectToAction("Index");
+                ViewBag.SuccessMessage = "Mesajiniz iletildi.";
+                return View();
             }
+
             ModelState.AddModelError("","Lütfen şikayet ve öneri alanını boş geçmeyiniz.");
             return View(complaint);
         }
